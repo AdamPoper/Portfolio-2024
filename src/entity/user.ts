7c33a0a0a96faf0e736d88166128cd75a07d3ff6
Adam Poper <adampoper@Adams-MacBook-Pro.local>
2024-08-08 23:24:59 -0400

    Adding user authentication

--- src/entity/user.ts
+++ src/entity/user.ts
@@ -1,11 +1,20 @@
 import { RowDataPacket } from "mysql2";
 
+export const USER_TABLE = 'User';
+
 export interface User extends RowDataPacket {
     id: number;
-    name: string;
     role: string;
+    username: string;
+    password: string;
+}
+
+export interface UserRequestBody {
+    username: string;
+    password: string;
 }
 
 export const UserQueries = {
-    GET_USER_BY_ID: "SELECT * FROM USER WHERE id = ?"
+    GET_USER_BY_ID: "SELECT * FROM USER WHERE id = ?",
+    GET_USER_BY_USERNAME: "SELECT * FROM USER WHERE username = ?",
 }
